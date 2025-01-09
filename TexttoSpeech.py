e3752c02b13c23fef20f8e9bca2346a165b9297b
rajeshaanand <rajesh.aanand@gmail.com>
2025-01-09 16:51:01 -0500

    Update TexttoSpeech.py

--- TexttoSpeech.py
+++ TexttoSpeech.py
@@ -1,7 +1,7 @@
 from pygame import mixer
 from gtts import gTTS
 def main():
-   tts = gTTS('Tendulkar took up cricket at the age of eleven, made his Test match debut on 15 November 1989 against Pakistan in Karachi at the age of sixteen, and went on to represent Mumbai domestically and India internationally for close to twenty-four years. ')
+   tts = gTTS('Tendulkar took up cricket at the age of eleve, made his Test match debut on 15 November 1989 against Pakistan in Karachi at the age of sixteen, and went on to represent Mumbai domestically and India internationally for close to twenty-four years. ')
    tts.save('output.mp3')
    mixer.init()
    mixer.music.load('output.mp3')
